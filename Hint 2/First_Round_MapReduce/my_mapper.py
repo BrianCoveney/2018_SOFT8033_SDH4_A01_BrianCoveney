75b2c553a0a77d3fabb1f376a449142df164286b
BrianCoveney <coveneybrian@gmail.com>
2018-04-01 12:17:52 +0100

    Complete Hint 2/First_Round_MapReduce/my_mapper.py

---
+++ Hint 2/First_Round_MapReduce/my_mapper.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+# --------------------------------------------------------
+#           PYTHON PROGRAM
+# Here is where we are going to define our set of...
+# - Imports
+# - Global Variables
+# - Functions
+# ...to achieve the functionality required.
+# When executing > python 'this_file'.py in a terminal,
+# the Python interpreter will load our program,
+# but it will execute nothing yet.
+# --------------------------------------------------------
+
+import codecs
+import sys
+
+
+# ---------------------------------------
+#  FUNCTION get_key_value
+# ---------------------------------------
+def get_key_value(line):
+    item_list = line.split(" ")
+    num_views = int(item_list[2])
+    return num_views
+
+
+# ---------------------------------------
+#  FUNCTION print_key_value
+# ---------------------------------------
+def print_key_value(output_stream, num_views, total):
+    res = "total " + '\t' + str(num_views + total) + '\n'
+    output_stream.write(res)
+
+
+# ------------------------------------------
+# FUNCTION my_map
+# ------------------------------------------
+def my_map(input_stream, output_stream):
+    total_count = 0
+    for text_line in input_stream.readlines():
+        num_views = get_key_value(text_line)
+        total_count += num_views
+
+    print_key_value(output_stream, num_views, total_count)
+
+    pass
+
+
+# ------------------------------------------
+# FUNCTION my_main
+# ------------------------------------------
+def my_main(debug, i_file_name, o_file_name):
+    # We pick the working mode:
+
+    # Mode 1: Debug --> We pick a file to read test the program on it
+    if debug == True:
+        my_input_stream = codecs.open(i_file_name, "r", encoding='utf-8')
+        my_output_stream = codecs.open(o_file_name, "w", encoding='utf-8')
+    # Mode 2: Actual MapReduce --> We pick std.stdin and std.stdout
+    else:
+        my_input_stream = sys.stdin
+        my_output_stream = sys.stdout
+
+    # We launch the Map program
+    my_map(my_input_stream, my_output_stream)
+
+
+# ---------------------------------------------------------------
+#           PYTHON EXECUTION
+# This is the main entry point to the execution of our program.
+# It provides a call to the 'main function' defined in our
+# Python program, making the Python interpreter to trigger
+# its execution.
+# ---------------------------------------------------------------
+if __name__ == '__main__':
+    # 1. Input parameters
+    debug = True
+
+    i_file_name = "../../my_dataset/pageviews-20180219-100000_1.txt"
+    o_file_name = "mapResult.txt"
+
+    # 2. Call to the function
+    my_main(debug, i_file_name, o_file_name)
